84fcb2a6be21124023674c4f1adfaacc704bc3fe
Skarlettmedina <148906923+Skarlettmedina@users.noreply.github.com>
2024-06-13 15:02:17 -0500

    todo backend

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,12 @@
 import { Router} from "express";
 import rutaUsuario from "./routes.usuario.js";
 import rutaProductos from "./routes.productos.js";
-import rutaDetalles from "./routes.detallecompra.js";
-import rutaCompras from "./routes.compras.js";
+
 
 const ruta = Router();
 
 ruta.use("/api", rutaUsuario);
 ruta.use("/api", rutaProductos);
-ruta.use("/api", rutaDetalles);
-ruta.use("/api", rutaCompras);
+
 
 export default ruta;
